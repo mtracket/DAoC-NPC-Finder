b08cbb96bfbfe7c3d14b44345ab8b277a0cebae4
Marco <Marco@192.168.178.112>
2018-09-10 23:42:26 +0200

    Alpha Preview - no functions just a bit of UI

---
+++ src/mt/daoc/npcfinder/FXMLNpcFinderMainController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mt.daoc.npcfinder;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Marco
+ */
+public class FXMLNpcFinderMainController implements Initializable {
+
+    @FXML
+    private AnchorPane MainWorkingPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void LoadJordheimMap(MouseEvent event) {
+    }
+    
+}
